fe5c4347ca7d71f6bf44f19b1d18be175a67e267
Sinchana-Rai <sinchana.srai@gmail.com>
2024-12-17 14:21:49 -0500

    fix for CORS origin error

--- app.js
+++ app.js
@@ -5,14 +5,31 @@ import cors from "cors";
 import contactRouter from "./router/contactRouter.js";
 import userRouter from "./router/userRouter.js";
 
+const allowedExtensions = [ 'http://localhost:5173',
+    'https://sr-cp-frontend.onrender.com' ];
+
 const app = express();
 
 dotenv.config({ path: "./config/config.env" });
 
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL, 
+//     methods: ["POST" , "DELETE" ], 
+//     credentials: true, 
+// }));
+
+//Updated to fix cors policy error
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL, 
-    methods: ["POST" , "DELETE" ], 
-    credentials: true, 
+    origin: (origin, callback) => {
+        if (allowedExtensions.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: "POST, DELETE ",
+    allowedHeaders: 'Content-Type,Authorization'
 }));
 
 app.use(express.json());
